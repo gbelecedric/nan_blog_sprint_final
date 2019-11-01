a59d122b23a3d6890da165b9217f66d18d76639d
Opeyemi19 <47104102+Opeyemi19@users.noreply.github.com>
2019-11-01 11:58:34 +0000

    commit settings

--- nanBlog/nanBlog/settings.py
+++ nanBlog/nanBlog/settings.py
@@ -88,10 +88,20 @@ WSGI_APPLICATION = 'nanBlog.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'groupecedricdb',
+        'USER': 'groupecedric',
+        'PASSWORD': 'nan0980',
+        'HOST': '51.77.197.177',
+        'PORT': '5432',
     }
+
 }
 
 
