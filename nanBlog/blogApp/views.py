450f4c7549601c605735cf1fb6142165458019b0
Dagoserge-martial <46801097+Dagoserge-martial@users.noreply.github.com>
2019-11-02 00:15:54 +0000

    recuperation

--- nanBlog/blogApp/views.py
+++ nanBlog/blogApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 def home(request):
@@ -54,12 +58,12 @@ def categorie(request, titre):
     return render(request, 'pages/blog/category.html',data)
 
 def dashbord(request):
-    # user = request.user
-    # print(user.username)
-    # print('***************')
-    # userpost = Article.objects.filter(nom=user)
-    # b = Article.objects.filter(nom=user, status=True)
-    # c = Article.objects.filter(nom=user, status=False)
+    user = request.user
+    print(user.username)
+    print('***************')
+    # userpost = Article.objects.filter(auteur=user)
+    # b = Article.objects.filter(auteur=user, status=True)
+    # c = Article.objects.filter(auteur=user, status=False)
     # bb = b.count()
     # a = userpost.count()
     # cc = c.count()
@@ -72,23 +76,23 @@ def dashbord(request):
     #     'bb': bb,
     #     'cc': cc,
     # }
-    return render(request, 'pages/dashbord/dashbord.html')
+    return render(request, 'pages/dashbord/dashbord.html', data)
 
 def dashpost(request):
-    # user = request.user
-    # print('+++++++++++++++',user,'+++++++++++++++++')
-    # userarticle = User.objects.all()
-    # #print(userarticle.ctegorieuser.articles.all)
-    # userpost = Article.objects.filter(nom=user)
+    user = request.user
+    print('+++++++++++++++',user,'+++++++++++++++++')
+    userarticle = User.objects.all()
+    #print(userarticle.ctegorieuser.articles.all)
+    userpost = Article.objects.filter(auteur=user)
     
 
-    # data={
-    #     'userarticle': userarticle,
-    #     'userpost': userpost,
+    data={
+        'userarticle': userarticle,
+        'userpost': userpost,
        
-    # }
+    }
 
-    return render(request, 'pages/dashbord/posts.html')
+    return render(request, 'pages/dashbord/posts.html', data)
 
 def dashdetail(request):
     
